9a7bf2bf60d28cafb70d30f86c52a47f97387ba3
Franze Carlo Natividad <111488970+Barbatosss@users.noreply.github.com>
2022-09-03 15:15:01 +0800

    Create App.java

---
+++ standard classs/App.java
@@ -0,0 +1,70 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character Saber = new Character();
+
+        Saber.strength = 10;
+        Saber.agility = 5;
+        Saber.intelligence = 0;
+        Saber.name = "Artoria Pendragon";
+        Saber.sayMyName();
+        Saber.sayMyStrength();
+        Saber.sayMyAgility();
+        Saber.sayMyIntelligence();
+
+        Character Lancer = new Character();
+
+        Lancer.strength = 0;
+        Lancer.agility = 5;
+        Lancer.intelligence = 10;
+        Lancer.name = "Cú Chulainn";
+        Lancer.sayMyName();
+        Lancer.sayMyStrength();
+        Lancer.sayMyIntelligence();
+
+        Character Archon = new Character();
+
+        Archon.strength = 5;
+        Archon.agility = 10;
+        Archon.intelligence = 0;
+        Archon.name = "Pransue";
+        Archon.sayMyName();
+        Archon.sayMyStrength();
+        Archon.sayMyAgility();
+        Archon.sayMyIntelligence();
+
+        Character archer = new Character();
+
+        archer.strength = 50;
+        archer.agility = 70;
+        archer.intelligence = 60;
+        archer.name = "Emiya Shirou";
+        archer.sayMyName();
+        archer.sayMyStrength();
+        archer.sayMyAgility();
+        archer.sayMyIntelligence();
+
+        Character Rider = new Character();
+        Rider.strength = 20;
+        Rider.agility = 20;
+        Rider.intelligence = 50;
+        Rider.name = "Medusa";
+        Rider.sayMyName();
+        Rider.sayMyStrength();
+        Rider.sayMyAgility();
+        Rider.sayMyIntelligence();
+
+        Character Teacher = new Character();
+        Teacher.strength = 50;
+        Teacher.agility = 20;
+        Teacher.intelligence = 10000;
+        Teacher.name = "Coli";
+        Teacher.sayMyName();
+        Teacher.sayMyStrength();
+        Teacher.sayMyAgility();
+        Teacher.sayMyIntelligence();
+            
+    
+
+    }
+}
