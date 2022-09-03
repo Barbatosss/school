36e06036aef296ef5a8d7fce7ba672e37f08f85d
Franze Carlo Natividad <111488970+Barbatosss@users.noreply.github.com>
2022-09-03 22:43:46 +0800

    Update App.java

--- Anime/App.java
+++ Anime/App.java
@@ -6,6 +6,12 @@ public class App{
     Anime eightysix = new Anime("eightysix", 23, 20, "Emma Anju", "Saori Hayami");
 
     Anime Jjk = new Anime("Jujutsu_Kaisen", 24, 20, "Nanami Kento", "Tsuda Kenjiro");
+
+    Anime VnK = new Anime("Vanitas no Karte", 24, 20, "Vanitas", "Hanae Natsuki");
+
+    Anime Gtm = new Anime("Gintama", 201, 20, "takasugi Shinsuke", "Koyasu Takehito");
+
+    Anime KnY = new Anime("KnY: Mugen Train", 1, 116, "Akaza", "Ishida Akira");
         
     }
 
