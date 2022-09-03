1672bdff2e520bb05e7fbed52b7ae80a670a89b6
Franze Carlo Natividad <111488970+Barbatosss@users.noreply.github.com>
2022-09-03 15:24:59 +0800

    Create Pet.java

---
+++ Pet/Pet.java
@@ -0,0 +1,15 @@
+public class Pet {
+    String name;
+    float trustlevel;
+    int petlevel;
+    boolean ishappy;
+    
+public Pet(String nam ,float tal,int pal,boolean ias){
+    name = nam;
+    trustlevel = tal;
+    petlevel = pal;
+    ishappy = ias;
+System.out.println("my name is " + name + "\n" + "trustlevel is " + trustlevel + "\n" + "level " + petlevel + "\n" + "happines " + ishappy);
+ }      
+
+}
