bc30f0ff8e200055a53a5efdb76e1a50c0c8b369
Franze Carlo Natividad <111488970+Barbatosss@users.noreply.github.com>
2022-09-03 15:24:29 +0800

    Create App.java

---
+++ Pet/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args){
+
+         Pet Cat = new Pet("wkwk", 2.2f, 1, true);
+
+         Pet riri = new Pet("bodo", 3.5f, 2, true);
+
+         Pet kodzu = new Pet("sia", 2.2f, 1, true);
+
+    }
+}
