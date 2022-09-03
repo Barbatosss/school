f0d8be375557a9daf4bb45ca310346730b4e9f92
Franze Carlo Natividad <111488970+Barbatosss@users.noreply.github.com>
2022-09-03 15:22:49 +0800

    Create App.java

---
+++ Item/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+
+        Item Homa  = new Item(900, 5.5f, 1, "legendary", "polearm", true);
+        
+        Item Kagura = new Item(600, 3.2f, 1, "used by miko", "catalyst", false);
+
+        Item JadeCutter = new Item(100, 2.2f, 1, "shogun's", "sword", true);
+
+
+
+
+
+    }
+}
