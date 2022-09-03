00eee1bb12cd85eb4395863232865753b85533ea
Franze Carlo Natividad <111488970+Barbatosss@users.noreply.github.com>
2022-09-03 22:32:17 +0800

    Create App.java

---
+++ Anime/App.java
@@ -0,0 +1,12 @@
+public class App{
+     public static void main(String[] args) {
+
+    Anime one_piece = new Anime("one piece", 1020, 20, "Monkey D. Luffy", "Mayumi Tanaka");
+
+    Anime eightysix = new Anime("eightysix", 23, 20, "Emma Anju", "Saori Hayami");
+
+    Anime Jjk = new Anime("Jujutsu_Kaisen", 24, 20, "Nanami Kento", "Tsuda Kenjiro");
+        
+    }
+
+}
