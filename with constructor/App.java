bd253bdf18e03449be54123235581f7b9c8865c8
Franze Carlo Natividad <111488970+Barbatosss@users.noreply.github.com>
2022-09-03 15:20:23 +0800

    Create App.java

---
+++ with constructor/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Creates a new object named "Knight" from Character class
+        Character knight = new Character(10, 5, 0, "Chen");
+       
+
+        // Creates a new object named "mage" from Character class
+        Character mage = new Character(0, 5, 10, "Akasha");
+        
+
+        // Creates a new object named "mage" from Character class
+        Character thief = new Character(5, 10, 0, "Gondar");
+        
+        
+        // Creates a new object named "doctor" from Character class
+        Character Phoenix = new Character(10, 10, 50, "Marco");
+        
+        // Creates a new object named "Navigator" from character class
+        Character Fire_Fist = new Character(10, 10, 50, "Ace");
+        
+
+
+    }
+}
